cce4697bc94a1e633a7f2e5d61ff8ff0c31f6da2
shafiqps <shafiqaimanps@gmail.com>
2022-06-14 22:38:09 +0800

    new scene

--- JavaFXApplication3/src/javafxapplication3/Controller.java
+++ JavaFXApplication3/src/javafxapplication3/Controller.java
@@ -1,13 +1,27 @@
 package javafxapplication3;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
+import javafx.stage.*;
+import javafx.scene.*;
+
+import java.awt.event.ActionEvent;
+import java.io.IOException;
 import java.util.*;
 public class Controller {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
     @FXML
     private Label welcomeText;
     @FXML
     private TextArea confessionPrint;
+
+    public Controller() {
+    }
 
 
     @FXML
@@ -23,5 +37,16 @@ public class Controller {
         confessionPrint.setText(JavaFXApplication3.confessionsTemp.peek());
         JavaFXApplication3.confessions.push(JavaFXApplication3.confessionsTemp.pop());
 
+    }
+
+    @FXML
+
+
+    public void switchScene(javafx.event.ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("makkau2.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
