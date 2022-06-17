e48cca5d51cbad5d61fd7e0f469f9a2b87ba8e4d
CAPANG JABBA <ihaziq18.ih@gmail.com>
2022-06-18 02:54:09 +0800

    UPDATE ALERT

--- JavaFXApplication3/src/javafxapplication3/DSNSFWFR.java
+++ JavaFXApplication3/src/javafxapplication3/DSNSFWFR.java
@@ -4,5 +4,5 @@ public class DSNSFWFR {
     public static String url = "jdbc:mysql://localhost:3306/confession_page_dsnsfwfr";
     public static String username = "root";
     public static String password = "Leham24";
-
+    // WENOMECHAINDESAMA
 }
